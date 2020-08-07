11752c86c6e3eb07c0f01a06d26ad90260894941
Rudyard Richter <rudyardrichter@uchicago.edu>
2020-08-07 14:28:28 -0500

    Better fail/error handling for missing token directory

--- timer_cli/auth.py
+++ timer_cli/auth.py
@@ -1,6 +1,7 @@
 import pathlib
 from typing import Optional
 
+import click
 from fair_research_login import JSONTokenStorage
 from fair_research_login.client import NativeClient
 
@@ -10,17 +11,36 @@ CLIENT_NAME = "timer_cli"
 SCOPES = [
     "https://auth.globus.org/scopes/524230d7-ea86-4a52-8312-86065a9e0417/transfer_action"
 ]
+TOKEN_DIRECTORY = f"{pathlib.Path.home()}/.config/globus"
+DEFAULT_TOKEN_FILE = str(
+    pathlib.Path(TOKEN_DIRECTORY).joinpath(pathlib.Path("tokens.json"))
+)
+
+
+def create_token_directory():
+    pathlib.Path(TOKEN_DIRECTORY).mkdir(parents=True, exist_ok=True)
 
 
 def get_access_token(token_store: Optional[str] = None):
     # use this rather than the actual default argument so we can chain multiple default
     # arguments (from `get_headers`)
-    token_store = token_store or f"{pathlib.Path.home()}/.config/globus/tokens.json"
-    cli = NativeClient(
-        client_id=CLIENT_ID,
-        app_name=CLIENT_NAME,
-        token_storage=JSONTokenStorage(token_store),
-    )
+    token_store = token_store or DEFAULT_TOKEN_FILE
+    try:
+        create_token_directory()
+        cli = NativeClient(
+            client_id=CLIENT_ID,
+            app_name=CLIENT_NAME,
+            token_storage=JSONTokenStorage(token_store),
+        )
+    except FileNotFoundError:
+        click.echo(
+            (
+                f"couldn't access or create file for token storage ({token_store});"
+                "make sure the CLI would be allowed to create the directory if it"
+                " doesn't exist, and/or open that file inside that directory"
+            ),
+            err=True
+        )
     cli.login(requested_scopes=SCOPES, refresh_tokens=True)
     authorizer = next(iter(cli.get_authorizers().values()))
     return authorizer.access_token
