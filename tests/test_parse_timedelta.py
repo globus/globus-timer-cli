6098fc5448dcad0f376b799caacfe11e920558f4
Rudyard Richter <rudyardrichter@uchicago.edu>
2021-01-12 13:04:34 -0600

    Add some whitespace-related test cases

--- tests/test_parse_timedelta.py
+++ tests/test_parse_timedelta.py
@@ -5,13 +5,17 @@ from timer_cli.main import _parse_timedelta
 
 test_cases = [
     ("", timedelta(seconds=0)),
+    ("      ", timedelta(seconds=0)),
     ("0", timedelta(seconds=0)),
     ("0s", timedelta(seconds=0)),
+    ("0   s", timedelta(seconds=0)),
     ("10", timedelta(seconds=10)),
     ("100", timedelta(seconds=100)),
     ("5m 10s", timedelta(minutes=5, seconds=10)),
     ("5m 100s", timedelta(minutes=5, seconds=100)),
     ("2h 3m 4s", timedelta(hours=2, minutes=3, seconds=4)),
+    ("2h3m4s", timedelta(hours=2, minutes=3, seconds=4)),
+    ("2h     3m   4s", timedelta(hours=2, minutes=3, seconds=4)),
     ("10h", timedelta(hours=10)),
 ]
 
